a33728b4f94f3184ac3bbe65bd353696f1946343
Yumiko Tezuka <ytezuka5@gmail.com>
2018-09-02 01:26:34 +0000

    challenge2tochu

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,18 +3,16 @@ class MessagesController < ApplicationController
     @message = Message.new
     @messages = Message.all
   end
-  
+
   def create
-    @message = Message.new
+    @message = Message.new(message_params)
     @message.save
-    redirect_to root_path, notice: 'メッセージを送信しました'
+    redirect_to root_path , notice: 'メッセージを保存しました'
   end
-  
+
   private
   def message_params
     params.require(:message).permit(:name, :body)
-  
   end
-
-  
+  ## ここまで
 end
